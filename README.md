73b01906903acd53ce58b1e8939cb0242abf23aa
muhalifalgibran <alifalgibran147@gmail.com>
2018-12-03 14:21:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# deliveryu" 
+"# deliveryu" 
---
+++ app/src/main/java/id/alif/delivery_u/SecondActivity.java
@@ -0,0 +1,13 @@
+package id.alif.delivery_u;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
